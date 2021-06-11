c97b8741d2e23c410c11ac8f899c74f98cfa1f32
CO16360 <poonervikas98@gmail.com>
2021-06-11 22:59:31 +0530

    added about me page

---
+++ src/components/Skill.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Skill = (props) => {
+   
+    return (
+        <div className="skill-container" >
+            {props.data.text}
+        </div>
+    )
+}
+
+export default Skill;
---
+++ src/information/Skills.js
@@ -0,0 +1,25 @@
+export const frontend_icon = "";
+export const backend_icon = "";
+export const tools_icon = "";
+
+
+export const SKILLS=[
+    {
+        icon:frontend_icon,
+        heading:"Frontend",
+        text:"I like to code things from scratch, and enjoy bringing ideas to life in the browser.",
+        skills:["HTML","CSS","Javascript","ReactJs","Angular","Bootstrap"]
+    },
+    {
+        icon:backend_icon,
+        heading:"Backend",
+        text:"I love creating API's that can be exposed and utilized well in the frontend.",
+        skills:["Java","SpringBoot","NodeJs"]
+    },
+    {
+        icon:tools_icon,
+        heading:"Tools",
+        text:"I use different tools day in and day out to pace up the process of development ",
+        skills:["Git","Wavemaker (LCD)","Bitbucket","Postman"]
+    }
+]
