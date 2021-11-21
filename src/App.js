47d7121b8fd53d31f109bbd8158a27029a444610
CO16360 <poonervikas98@gmail.com>
2021-11-21 20:19:05 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './styles/Main.css';
 import About from './components/About';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,HashRouter } from 'react-router-dom';
 import Journey from './components/Journey';
 import Blogs from './components/Blogs';
 
 function App() {
   return (
     <div className="dark-theme">
-      <Router >
+      <HashRouter >
         <Navigation />
         <Switch>
 
           <Route exact path="/" component={Landing} />
-          <Route path="/about" component={About} />
-          <Route path="/timeline" component={Journey} />
-          <Route path="/blog" component={Blogs} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/timeline" component={Journey} />
+          <Route exact path="/blog" component={Blogs} />
         </Switch>
 
-      </Router>
+      </HashRouter>
      
     </div>
 
