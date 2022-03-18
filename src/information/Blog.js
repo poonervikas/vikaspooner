2ab87541531d46e0d468d37f401cd9a9b3eead91
poonervikas <poonervikas98@gmail.com>
2022-03-18 23:17:03 +0530

    added infor for meta reducers & lodash blog

--- src/information/Blog.js
+++ src/information/Blog.js
@@ -6,6 +6,22 @@ export const BLOGSDATA=[
         "date":"November 13, 2021",
         "brief":"Database Access in Java using JDBC in 6 steps.",
         "tags":["Java","JDBC","Architecture","JDBC Driver","Database"]
+    },
+    {
+        "title":"Ins & Outs of Meta reducers in Ngrx",
+        "link":"https://medium.com/@poonervikas98/ins-outs-of-meta-reducers-in-ngrx-with-examples-f68a9958640b",
+        "readTime":"5 min",
+        "date":"March 12, 2022",
+        "brief":"Practical implementation of meta-reducers in Ngrx with examples.",
+        "tags":["Angular","Ngrx","Meta-Reducers","State Management","Local Storage"]
+    },
+    {
+        "title":"Introduction to Lodash and its frequently used methods",
+        "link":"https://medium.com/@poonervikas98/introduction-to-lodash-and-its-frequently-used-methods-6810deb5b84e",
+        "readTime":"3 min",
+        "date":"March 13, 2022",
+        "brief":"Working with Lodash methods along with code snippets.",
+        "tags":["Javascript","Angular","Lodash","Library","Underscore.js"]
     }
     //,
     // {
